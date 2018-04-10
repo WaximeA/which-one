7e57a8d6f2353372c0cd8b849af0561ac34e60aa
WaximeA <aveline.maxime@gmail.com>
2018-04-10 22:29:17 +0200

    #entity : add face and faceCategory entities

---
+++ src/AppBundle/Entity/Face.php
@@ -0,0 +1,393 @@
+<?php
+
+namespace AppBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Validator\Constraints as Assert;
+use Symfony\Component\HttpFoundation\File\File;
+use Vich\UploaderBundle\Mapping\Annotation as Vich;
+
+/**
+ * Face
+ *
+ * @ORM\Table(name="face")
+ * @ORM\Entity(repositoryClass="AppBundle\Repository\FaceRepository")
+ * @Vich\Uploadable
+ */
+class Face
+{
+    /**
+     * @var int
+     *
+     * @ORM\Column(name="id", type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    private $id;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="titre", type="string", length=255)
+     */
+    private $titre;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="content", type="text", length=255, nullable=true)
+     */
+    private $content;
+    /**
+     * @var File
+     *
+     * @ORM\Column(name="image", type="string", length=255, nullable=true)
+     * @Assert\File(mimeTypes={ "image/jpeg", "image/png", "image/gif" })
+     * @Vich\UploadableField(mapping="face_image", fileNameProperty="imageName")
+     *
+     */
+    private $image;
+
+    /**
+     * @ORM\Column(type="string", length=255, nullable=true)
+     *
+     * @var string
+     */
+    private $imageName;
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="url_video", type="string", length=255, nullable=true)
+     */
+    private $urlVideo;
+
+    /**
+     * @var bool
+     *
+     * @ORM\Column(name="enable", type="boolean", nullable=true)
+     */
+    private $enable;
+
+    /**
+     * @var \DateTime
+     *
+     * @ORM\Column(name="createdAt", type="datetime", nullable=true)
+     */
+    private $createdAt;
+
+    /**
+     * @ORM\ManyToOne(targetEntity="FaceCategory")
+     */
+    private $category;
+
+    /**
+     * @ORM\ManyToMany(targetEntity="User", inversedBy="proposals")
+     */
+    private $users;
+
+
+    /**
+     * @var integer
+     *
+     * @ORM\Column(name="nbVote", type="integer", nullable=true)
+     */
+    private $nbVote;
+
+
+
+    /**
+     * Get id
+     *
+     * @return int
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set titre
+     *
+     * @param string $titre
+     *
+     * @return Face
+     */
+    public function setTitre($titre)
+    {
+        $this->titre = $titre;
+
+        return $this;
+    }
+
+    /**
+     * Get titre
+     *
+     * @return string
+     */
+    public function getTitre()
+    {
+        return $this->titre;
+    }
+
+    /**
+     * Set image
+     *
+     * @param string $image
+     *
+     * @return Face
+     */
+    public function setImage($image)
+    {
+        $this->image = $image;
+
+        if (null !== $image) {
+            // It is required that at least one field changes if you are using doctrine
+            // otherwise the event listeners won't be called and the file is lost
+            $this->createdAt = new \DateTimeImmutable();
+        }
+        return $this;
+    }
+
+    /**
+     * Get image
+     *
+     * @return string
+     */
+    public function getImage()
+    {
+        return $this->image;
+    }
+
+    /**
+     * Set urlVideo
+     *
+     * @param string $urlVideo
+     *
+     * @return Face
+     */
+    public function setUrlVideo($urlVideo)
+    {
+        $this->urlVideo = $urlVideo;
+
+        return $this;
+    }
+
+    /**
+     * Get urlVideo
+     *
+     * @return string
+     */
+    public function getUrlVideo()
+    {
+        return $this->urlVideo;
+    }
+
+    /**
+     * Set enable
+     *
+     * @param boolean $enable
+     *
+     * @return Face
+     */
+    public function setEnable($enable)
+    {
+        $this->enable = $enable;
+
+        return $this;
+    }
+
+    /**
+     * Get enable
+     *
+     * @return bool
+     */
+    public function getEnable()
+    {
+        return $this->enable;
+    }
+
+    /**
+     * Set createdAt
+     *
+     * @param \DateTime $createdAt
+     *
+     * @return Face
+     */
+    public function setCreatedAt($createdAt)
+    {
+        $this->createdAt = $createdAt;
+
+        return $this;
+    }
+
+    /**
+     * Get createdAt
+     *
+     * @return \DateTime
+     */
+    public function getCreatedAt()
+    {
+        return $this->createdAt;
+    }
+
+    /**
+     * Set text
+     *
+     * @param string $text
+     *
+     * @return Face
+     */
+    public function setText($text)
+    {
+        $this->text = $text;
+
+        return $this;
+    }
+
+    /**
+     * Get text
+     *
+     * @return string
+     */
+    public function getText()
+    {
+        return $this->text;
+    }
+
+    /**
+     * Set content
+     *
+     * @param string $content
+     *
+     * @return Face
+     */
+    public function setContent($content)
+    {
+        $this->content = $content;
+
+        return $this;
+    }
+
+    /**
+     * Get content
+     *
+     * @return string
+     */
+    public function getContent()
+    {
+        return $this->content;
+    }
+
+    /**
+     * Set imageName
+     *
+     * @param string $imageName
+     *
+     * @return Face
+     */
+    public function setImageName($imageName)
+    {
+        $this->imageName = $imageName;
+
+        return $this;
+    }
+
+    /**
+     * Get imageName
+     *
+     * @return string
+     */
+    public function getImageName()
+    {
+        return $this->imageName;
+    }
+
+    /**
+     * Set category
+     *
+     * @param \AppBundle\Entity\FaceCategory $category
+     *
+     * @return Face
+     */
+    public function setCategory(\AppBundle\Entity\FaceCategory $category = null)
+    {
+        $this->category = $category;
+
+        return $this;
+    }
+
+    /**
+     * Get category
+     *
+     * @return \AppBundle\Entity\FaceCategory
+     */
+    public function getCategory()
+    {
+        return $this->category;
+    }
+    /**
+     * Constructor
+     */
+    public function __construct()
+    {
+        $this->users = new \Doctrine\Common\Collections\ArrayCollection();
+    }
+
+    /**
+     * Set nbVote
+     *
+     * @param integer $nbVote
+     *
+     * @return Face
+     */
+    public function setNbVote($nbVote)
+    {
+        $this->nbVote = $nbVote;
+
+        return $this;
+    }
+
+    /**
+     * Get nbVote
+     *
+     * @return integer
+     */
+    public function getNbVote()
+    {
+        return $this->nbVote;
+    }
+
+    /**
+     * Add user
+     *
+     * @param \AppBundle\Entity\User $user
+     *
+     * @return Face
+     */
+    public function addUser(\AppBundle\Entity\User $user)
+    {
+        $this->users[] = $user;
+
+        return $this;
+    }
+
+    /**
+     * Remove user
+     *
+     * @param \AppBundle\Entity\User $user
+     */
+    public function removeUser(\AppBundle\Entity\User $user)
+    {
+        $this->users->removeElement($user);
+    }
+
+    /**
+     * Get users
+     *
+     * @return \Doctrine\Common\Collections\Collection
+     */
+    public function getUsers()
+    {
+        return $this->users;
+    }
+}
